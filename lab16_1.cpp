38eb4c2dce2237f2fc5baabb0f3ae200b82a4d94
unknown <pomllpp005@gmail.com>
2025-01-31 20:18:05 +0700

    Finish

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -32,3 +32,34 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+void randData(double *a ,int N ,int M){
+    for(int i = 0; i < N*M; i++){
+        *(a + i) = (rand() % 100)*0.01;
+    }
+}
+void showData(double *a, int N, int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << *(a + i);
+        if((i + 1) % M == 0) cout << endl;
+        else cout << " ";
+    }
+}
+void findRowSum(const double *a,double *b,int N,int M){
+    for(int i=0;i<N;i++){
+        b[i] = 0;
+        for(int j=0;j<M;j++){
+        b[i]+= *(a + i * M + j); 
+        }
+        
+    }
+}
+void findColSum(const double *a,double *b,int N,int M){
+    for(int i=0;i<M;i++){
+        b[i] = 0;
+        for(int j=0;j<N;j++){
+        b[i]+= *(a + j*M+i); 
+        }
+        
+    }
+}
